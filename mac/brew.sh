1642f5298f849b27c94bf63101f54401d4db0de9
Jim Wallace <jim@extroverteddeveloper.com>
2019-03-19 22:45:14 -0400

    gpg was missing

--- mac/brew.sh
+++ mac/brew.sh
@@ -56,6 +56,7 @@ brew install woff2
 
 # Install other useful binaries.
 brew install git
+brew install gpg
 
 
 # Development tool casks
@@ -65,8 +66,8 @@ brew cask install --appdir="/Applications" 1password
 brew cask install powershell
 # Change to the new shell, prompts for password
 sudo bash -c 'echo /usr/local/bin/pwsh >> /etc/shells'
-/usr/local/bin/pwsh -Command Install-Module PSColor
-/usr/local/bin/pwsh -Command Install-Module posh-git
+/usr/local/bin/pwsh -Command Install-Module -Confirm PSColor
+/usr/local/bin/pwsh -Command Install-Module -Confirm posh-git
 chsh -s /usr/local/bin/pwsh
 
 brew install terraform
