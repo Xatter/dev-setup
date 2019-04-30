b6f03e537e64a502904f66269c91081bb6f27a47
Jim Wallace <jim@extroverteddeveloper.com>
2019-04-30 10:20:15 -0400

    beginning of windows setup

---
+++ windows/windows.ps1
@@ -0,0 +1,6 @@
+# Set fast keyboard repeat
+Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatDelay -Value 200
+Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatRate -Value 6
+Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name DelayBeforeAcceptance -Value 0
+Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name Flags -Value 59
+Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name BounceTime -Value 0
