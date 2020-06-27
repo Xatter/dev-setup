2437d16878ba4965a7b7d7a3b4a69a36a3d0da6d
Jim Wallace <jim@extroverteddeveloper.com>
2020-06-27 10:24:24 -0400

    add oh-my-zsh and zshrc to setup

--- mac/terminal/setup.sh
+++ mac/terminal/setup.sh
@@ -3,10 +3,8 @@
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Enable VIM keybindings for command line in zsh
-echo "bindkey -v" >> ~/.zshrc
-
 # Install my ZSH Theme
-ln -s ./jim.zsh-theme ~/.oh-my-zsh/themes/jim.zsh-theme
-cat ~/.zshrc | sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="jim"/' > ~/.zshrc
+ln -s $(pwd)/jim.zsh-theme ~/.oh-my-zsh/themes/jim.zsh-theme
+rm ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc
 
