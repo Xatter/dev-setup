53f39ae992360e41c39cec52c976cef755b85d8b
Jim Wallace <jim@extroverteddeveloper.com>
2020-10-13 09:51:37 -0400

    add git branch configuration

--- mac/git.sh
+++ mac/git.sh
@@ -20,6 +20,21 @@ git config --global merge.tool diffmerge
 git config --global mergetool.diffmerge.trustExitCode true
 git config --global mergetool.diffmerge.cmd '/usr/local/bin/diffmerge -result="$MERGED" "$LOCAL" "$BASE" "$REMOTE"'
 
+echo "Setting up Kaleidoscope"
+# curl -sSL -k https://updates.blackpixel.com/latest\?app\=ksdiff\&v\=122 -o ksdiff-122-2.2.0.zip
+# unzip ksdiff-122-2.2.0.zip
+
+git config --global diff.tool 'Kaleidoscope'
+git config --global merge.tool 'Kaleidoscope'
+git config --global mergetool.Kaleidoscope.cmd 'ksdiff --merge --output "$MERGED" --base "$BASE" -- "$LOCAL" --snapshot "$REMOTE" --snapshot'
+git config --global mergetool.Kaleidoscope.trustexitcode truek
+git config --global difftool.Kaleidoscope.cmd 'ksdiff --partial-changeset --relative-path "$MERGED" -- "$LOCAL" "$REMOTE"'
+git config --global difftool.prompt false
+git config --global mergetool.prompt false
+
+
 echo "Setting up author"
 git config --global user.name "Jim Wallace"
 git config --global user.email "jim@extroverteddeveloper.com"
+
+git config --global pager.branch false
