8ee1dadfc2c9d3c48bf2b6dcd2523386d1c768ee
Jim Wallace <jim@extroverteddeveloper.com>
2021-01-29 14:33:09 -0500

    move .zshrc here

--- zsh.sh
+++ zsh.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Install Oh My Zsh
-curl -fsSLO https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-chmod a+x install.sh
-./install.sh
-
 # Install my ZSH Theme
 ln -s $(pwd)/jim.zsh-theme ~/.oh-my-zsh/themes/jim.zsh-theme
 rm ~/.zshrc
 ln -s $(pwd)/.zshrc ~/.zshrc
-rm install.sh
+
+# Install Oh My Zsh
+curl -fsSLO https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
