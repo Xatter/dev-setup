471fb32c3d8506db2db91b059b1e4f3399ef1f82
Jim Wallace <jim@extroverteddeveloper.com>
2021-02-01 17:49:11 -0500

    this is the way

--- zsh.sh
+++ zsh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Install Oh My Zsh
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+
 # Install my ZSH Theme
 ln -s $(pwd)/jim.zsh-theme ~/.oh-my-zsh/themes/jim.zsh-theme
 rm ~/.zshrc
 ln -s $(pwd)/.zshrc ~/.zshrc
 
-# Install Oh My Zsh
-curl -fsSLO https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
